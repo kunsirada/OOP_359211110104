6732c31e1e00a314669de530358c28c4a235076b
kunsirada <mummy_345@hotmail.com>
2017-11-17 11:14:09 +0700

    successfuly information data

--- src/oop_lab/s359211110104/rmutsv/com/myFirstJAVA.java
+++ src/oop_lab/s359211110104/rmutsv/com/myFirstJAVA.java
@@ -4,5 +4,8 @@ public class myFirstJAVA {
 
     public static void main(String[] args) {
         System.out.println("Hello World I'm PRIM");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
     }//main
 }//class

8465d3d1ac1d073c51a538fc699177ead0aa26aa
kunsirada <mummy_345@hotmail.com>
2017-11-17 10:31:39 +0700

    Testing upload to Github

---
+++ src/oop_lab/s359211110104/rmutsv/com/myFirstJAVA.java
@@ -0,0 +1,8 @@
+package oop_lab.s359211110104.rmutsv.com;
+
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World I'm PRIM");
+    }//main
+}//class
